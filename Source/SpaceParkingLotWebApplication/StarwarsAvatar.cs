223540915b4baa168c8a6c5cd2fd6c2b3a2c3993
andreasbrunnstrom <best_i25@hotmail.com>
2021-03-18 20:12:29 +0100

    Mapped_StarwarsAvatar
    await/async need looked at aswell as Data and Data.Result

--- Source/SpaceParkingLotWebApplication/StarwarsAvatar.cs
+++ Source/SpaceParkingLotWebApplication/StarwarsAvatar.cs
@@ -7,5 +7,24 @@ namespace SpaceParkingLotWebApplication
 {
     public class StarwarsAvatar
     {
+        public List<StarwarsAvatar> Data { get; set; }
+
+        public string name { get; set; }
+        public int height { get; set; }
+        public int mass { get; set; }
+        public string hair_color { get; set; }
+        public string skin_color { get; set; }
+        public string eye_color { get; set; }
+        public string birth_year { get; set; }
+        public string gender { get; set; }
+        public Uri homeworld { get;set;}
+        public Uri[] films { get; set; }
+        public Uri[] species { get; set; }
+        public Uri[] vehicles { get; set; }
+        public Uri[] starships { get; set; }
+        public DateTime created { get; set; }
+        public DateTime edited { get; set; }
+        public Uri url { get; set; }
+
     }
 }
