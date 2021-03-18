361ce6ba135c2be16e902882c667bb50839f658d
andreasbrunnstrom <best_i25@hotmail.com>
2021-03-18 16:18:05 +0100

    Created_classFetchFromAPI

---
+++ Source/SpaceParkingLotWebApplication/FetchFromStarwarsAPI.cs
@@ -0,0 +1,22 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceParkingLotWebApplication
+{
+    public class FetchFromStarwarsAPI
+    {
+        var client = new RestClient("https://swapi.dev/api/");
+        var request = new RestRequest("people/", DataFormat.Json);
+        // NOTE: The Swreponse is a custom class which represents the data returned by the API, RestClient have buildin ORM which maps the data from the reponse into a given type of object
+        var peopleResponse = await client.GetAsync<Swreponse>(request);
+
+        Console.WriteLine(peopleResponse.Data.Count);
+        foreach (var p in peopleResponse.Data.Results)
+        {
+            Console.WriteLine(p.Name);
+        }
+}
+
