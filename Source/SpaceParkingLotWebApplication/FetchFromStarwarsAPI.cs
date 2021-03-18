8343dfe7d0ca18fc117955ca7468cf7a672ca69d
andreasbrunnstrom <best_i25@hotmail.com>
2021-03-18 19:53:11 +0100

    Created_StarwarsPersonClass
    And incapsulated RestCharp

--- Source/SpaceParkingLotWebApplication/FetchFromStarwarsAPI.cs
+++ Source/SpaceParkingLotWebApplication/FetchFromStarwarsAPI.cs
@@ -8,15 +8,19 @@ namespace SpaceParkingLotWebApplication
 {
     public class FetchFromStarwarsAPI
     {
-        var client = new RestClient("https://swapi.dev/api/");
-        var request = new RestRequest("people/", DataFormat.Json);
-        // NOTE: The Swreponse is a custom class which represents the data returned by the API, RestClient have buildin ORM which maps the data from the reponse into a given type of object
-        var peopleResponse = await client.GetAsync<Swreponse>(request);
-
-        Console.WriteLine(peopleResponse.Data.Count);
-        foreach (var p in peopleResponse.Data.Results)
+        public FetchFromStarwarsAPI()
         {
-            Console.WriteLine(p.Name);
-        }
+            var client = new RestClient("https://swapi.dev/api/");
+            var request = new RestRequest("people/", DataFormat.Json);
+            // NOTE: The Swreponse is a custom class which represents the data returned by the API, RestClient have buildin ORM which maps the data from the reponse into a given type of object
+            var peopleResponse = await client.GetAsync<StarwarsAvatar>(request);
+
+            Console.WriteLine(peopleResponse.Data.Count);
+            foreach (var p in peopleResponse.Data.Results)
+            {
+                Console.WriteLine(p.Name);
+            }
+        }       
+    }
 }
 
