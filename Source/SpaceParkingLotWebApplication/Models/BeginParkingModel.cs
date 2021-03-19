110cb937dfcd5f0801e4a3d09656584964f4c69f
Daniel Runnvik <d.runnvik@gmail.com>
2021-03-19 14:30:20 +0100

    testade att bygga en sida med input för parkering, ska kolla vidare hur dert lagras

---
+++ Source/SpaceParkingLotWebApplication/Models/BeginParkingModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceParkingLotWebApplication.Models
+{
+    public class BeginParkingModel
+    {
+        public string Name { get; set; }
+
+        public int VehicleID { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime Endtime { get; set; }
+    }
+}
---
+++ Source/SpaceParkingLotWebApplication/Pages/Forms/BeginParking.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SpaceParkingLotWebApplication.Models;
+
+namespace SpaceParkingLotWebApplication.Pages.Forms
+{
+    public class BeginParking : PageModel
+    {
+        [BindProperty]
+        public BeginParkingModel Parking { get; set; }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("./index");
+
+        }
+    }
+}
