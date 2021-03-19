4a11ea0b65e9b7f4849631e8362490040a3824bd
Magnusson <Magnusson-5@hotmail.com>
2021-03-19 12:07:48 +0100

    Added a button with OnClickEvent()

--- Source/SpaceParkingLotWebApplication/Pages/Index.cshtml.cs
+++ Source/SpaceParkingLotWebApplication/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SpaceParkingLotWebApplication.Models;
 
 namespace SpaceParkingLotWebApplication.Pages
 {
@@ -17,12 +18,25 @@ namespace SpaceParkingLotWebApplication.Pages
             _logger = logger;
         }
 
+        public string Message { get; set; }
+
+
         public List<string> randomList = new List<string>() { "Hund", "Katt", "Yoda", "Luke Skywalker", "Arsenal" };
         public string[] randomList2 = { "Hund", "Katt", "Yoda", "Luke Skywalker", "Arsenal" };
 
+
+
+        public StarwarsAvatar avatar { get; set; }
+
+        //avatar = FetchAsyncFromAPI();
+
         public void OnGet()
         {
-
+            Message = "Get used";
+        }
+        public void OnPost()
+        {
+            Message = "Post used";
         }
     }
 }
