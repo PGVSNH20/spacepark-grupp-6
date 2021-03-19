f9fef36252c8a656e36fcfb25f1b393e10ea9abe
Daniel Runnvik <d.runnvik@gmail.com>
2021-03-19 16:16:35 +0100

    Validering av input Name mot Swapi fungerar nu.

--- Source/SpaceParkingLotWebApplication/Pages/Forms/BeginParking.cshtml.cs
+++ Source/SpaceParkingLotWebApplication/Pages/Forms/BeginParking.cshtml.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RestSharp;
 using SpaceParkingLotWebApplication.Models;
 
 namespace SpaceParkingLotWebApplication.Pages.Forms
 {
     public class BeginParking : PageModel
     {
+        public List<StarwarsAvatar> starWarsUniverseAvatars = FetchStarWarsAvatarsAsync().Result;
+
+        static async Task<List<StarwarsAvatar>> FetchStarWarsAvatarsAsync()
+        {
+            List<StarwarsAvatar> avatars = new List<StarwarsAvatar>();
+
+            var client = new RestClient("https://swapi.dev/api/");
+            var request = new RestRequest("people/", DataFormat.Json);
+            var peopleResponse = await client.GetAsync<StarWarsUniverse>(request);
+
+            foreach (var p in peopleResponse.results)
+            {
+                avatars.Add(p);
+            }
+
+            return avatars;
+        }
+
         [BindProperty]
         public BeginParkingModel Parking { get; set; }
         public void OnGet()
@@ -22,8 +41,15 @@ namespace SpaceParkingLotWebApplication.Pages.Forms
             {
                 return Page();
             }
+            if (starWarsUniverseAvatars.Any(x => x.name == Parking.Name))
+                {
+                return RedirectToPage("/index");
+               }
+            return RedirectToPage("/error");
 
-            return RedirectToPage("/index");
+            //Save Model to DataBase
+
+            //return RedirectToPage("/index");
 
         }
     }
