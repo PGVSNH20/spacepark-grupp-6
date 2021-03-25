90c5259413e6bb0179bd4fbcb9ba4101a25de9f8
Magnusson <Magnusson-5@hotmail.com>
2021-03-25 17:08:51 +0100

    Cleaned up some usings

--- Source/SpaceParkingLotWebApplication/Program.cs
+++ Source/SpaceParkingLotWebApplication/Program.cs
@@ -1,13 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using RestSharp;
-using SpaceParkingLotWebApplication.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SpaceParkingLotWebApplication
 {
--- Source/SpaceParkingLotWebApplication/Startup.cs
+++ Source/SpaceParkingLotWebApplication/Startup.cs
@@ -2,14 +2,9 @@ using EFDataAccessLibrary.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SpaceParkingLotWebApplication
 {
