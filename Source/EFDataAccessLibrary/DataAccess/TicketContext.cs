bcb3d0207341d8fa46a4e187d6720e0bff355240
Magnusson <Magnusson-5@hotmail.com>
2021-03-26 11:17:54 +0100

    Added method to To TicketContext

--- Source/EFDataAccessLibrary/DataAccess/TicketContext.cs
+++ Source/EFDataAccessLibrary/DataAccess/TicketContext.cs
@@ -8,8 +8,16 @@ namespace EFDataAccessLibrary.DataAccess
 {
     public class TicketContext : DbContext
     {
-        public TicketContext(DbContextOptions options) : base(options) { }
+        //public TicketContext(DbContextOptions options) : base(options) { }
 
         public DbSet<TicketRecord> Tickets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Connection string examples for SQL server
+            // https://www.connectionstrings.com/sql-server/
+            // Standard Security: Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;
+            optionsBuilder.UseMySql("Server=85.227.174.43;Database=lukeskywalker;Uid=lukeskywalker;Pwd=Vader56;", new MySqlServerVersion(new System.Version(5, 0, 12)));
+        }
     }
 }
