842e69d1405429b3d9d3e8f6a2515735abd065bb
Daniel Runnvik <d.runnvik@gmail.com>
2021-03-27 22:27:28 +0100

    försökte fixa med utseendet lite, blev nog mest bara en jobbig loop av starwars theme.

--- Source/SpaceParkingLotWebApplication/wwwroot/js/site.js
+++ Source/SpaceParkingLotWebApplication/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// TESTAR LITE// DR
+/* 
+The following JS gets the audio element and sets the start volume for the main Star Wars theme.
+*/
+
+window.onload = function () {
+    var backgroundAudio = document.getElementById("bgAudio");
+
+    backgroundAudio.volume = 0.1;
+
+    // second seek to the specific time you're looking for
+    // backgroundAudio.currentTime = 0;
+}
